b126269a2e3bdbce98eb652ed65b2862ddc246d1
Aaron Munguia <aamm89@gmail.com>
2014-05-28 02:25:08 -0500

    use ui.boostrap, set nickname and improve socket handle events

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,29 +1,69 @@
 // Angular way ej: https://www.youtube.com/watch?v=6bXpq1xiDsQ
-var app = angular.module('awesomeChat', []);
+var app = angular.module('awesomeChat', ['ui.bootstrap']);
 
 app.factory('socket', function(){
     var socket = io.connect('/');
     return socket;
 });
 
-app.controller('ChatCtrl', function($scope, socket){
+app.controller('ChatCtrl', function($scope, socket, $modal){
     $scope.messages = [];
 
+    var nameModal = $modal.open({
+        templateUrl: 'nameModal.html',
+        controller: nameModalCtrl,
+        backdrop: 'static'
+    });
+
+    nameModal.result.then(function(nickname){
+        $scope.nickname = nickname;
+        socket.emit('set_name', {name: nickname});
+    });
+
     $scope.sendMsg = function(){
-        var data = {
+        var message = {
             text : $scope.message.text,
             type: 'userMessage'
         };
-        socket.send(JSON.stringify(data));
+        socket.send(JSON.stringify(message));
         $scope.message.text = '';
     };
 
-    socket.on("message", function(data){
-        data = JSON.parse(data);
-        $scope.messages.push(data);
+    socket.on("name_set", function(user){
+        var message = {
+            text: 'Hello ' + user.name,
+            type: 'systemMessage'
+        };
+        $scope.messages.push(message);
         $scope.$digest();
+
+        // Handle message event 
+        socket.on("message", function(message){
+            message = JSON.parse(message);
+            $scope.messages.push(message);
+            $scope.$digest();
+        });
+
+        // Handle user_entered event 
+        socket.on("user_entered", function(user){
+            var message = {
+                text: user.name + ' has joined the room.',
+                type: 'systemMessage'
+            };
+            $scope.messages.push(message);
+            $scope.$digest();
+        });
     });
 });
+
+var nameModalCtrl = function($scope, $modalInstance){
+    $scope.user = {
+        nickname: ''
+    };
+    $scope.ok = function(){
+        $modalInstance.close($scope.user.nickname);
+    };
+};
 
 
 // jQuery way
