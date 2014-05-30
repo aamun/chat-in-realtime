c80e535b64ec2fe241e62ae1672caec60bc1c4a5
Aaron Munguia <aamm89@gmail.com>
2014-05-30 02:18:03 -0500

    use ngRoute and controllers, new factory for socket, add messages and user models

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,24 +1,121 @@
 // Angular way ej: https://www.youtube.com/watch?v=6bXpq1xiDsQ
-var app = angular.module('awesomeChat', ['ui.bootstrap']);
 
-app.factory('socket', function(){
-    var socket = io.connect('/');
-    return socket;
+// app.js
+var app = angular.module('chatApp', ['ngRoute', 'chatControllers']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+    when('/', {
+        templateUrl: 'partials/index.html',
+        controller: 'IndexCtrl'
+    }).
+    when('/chatroom', {
+        templateUrl: 'partials/chatroom.html',
+        controller: 'ChatCtrl'
+    }).
+    otherwise({
+        redirecTo: '/'
+    });
+}]);
+
+// Controllers
+
+// controllers.js
+var chatControllers = angular.module('chatControllers', []);
+
+// Socket factory
+// Notice that we wrap each socket callback in $scope.$apply. 
+// This tells AngularJS that it needs to check the state of 
+// the application and update the templates if there was a 
+// change after running the callback passed to it. Internally, 
+// $http works in the same way; after some XHR returns, 
+// it calls $scope.$apply, so that AngularJS can update its 
+// views accordingly.
+chatControllers.factory('socket', ['$rootScope',function ($rootScope) {
+  var socket = io.connect('/');
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () { // Tell 
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    },
+    send: function(message){
+        socket.send(message);
+    }
+  };
+}]);
+
+// Messages factory
+chatControllers.factory('messages', function(){
+    return [];
 });
 
-app.controller('ChatCtrl', function($scope, socket, $modal){
-    $scope.messages = [];
+// User factory
+chatControllers.factory('user', function(){
+    return {nickname : ''};
+});
 
-    var nameModal = $modal.open({
-        templateUrl: 'nameModal.html',
-        controller: nameModalCtrl,
-        backdrop: 'static'
+// Main Controller
+chatControllers.controller('MainCtrl', function($scope, $location, socket, user, messages){
+    
+    $scope.messages = messages;
+    $scope.user = user;
+
+    // Init (Login) user set nickname
+    socket.on("init", function(user){
+        var message = {
+            text: 'Hello ' + user.name,
+            type: 'systemMessage'
+        };
+        $scope.messages.push(message);
+
+        // Redirect to chatroom
+        $location.path('/chatroom');
     });
 
-    nameModal.result.then(function(nickname){
-        $scope.nickname = nickname;
-        socket.emit('set_name', {name: nickname});
+    // Handle message event 
+    socket.on("message", function(message){
+        message = JSON.parse(message);
+        $scope.messages.push(message);
     });
+
+    // Handle user:join event 
+    socket.on("user:join", function(user){
+        var message = {
+            text: user.name + ' has joined the room.',
+            type: 'systemMessage'
+        };
+        $scope.messages.push(message);
+    });
+});
+
+// Index Controller child of Main Controller
+chatControllers.controller('IndexCtrl', function($scope, $location, socket){
+
+    $scope.setName = function(){
+        socket.emit('change:name', {name: $scope.user.nickname});
+    };
+});
+
+// Chat Controller child of Main Controller
+chatControllers.controller('ChatCtrl', function($scope, $location, socket){
+    
+    if ($scope.user.nickname === '') {
+        $location.path('/');
+    }
 
     $scope.sendMsg = function(){
         var message = {
@@ -28,65 +125,4 @@ app.controller('ChatCtrl', function($scope, socket, $modal){
         socket.send(JSON.stringify(message));
         $scope.message.text = '';
     };
-
-    socket.on("name_set", function(user){
-        var message = {
-            text: 'Hello ' + user.name,
-            type: 'systemMessage'
-        };
-        $scope.messages.push(message);
-        $scope.$digest();
-
-        // Handle message event 
-        socket.on("message", function(message){
-            message = JSON.parse(message);
-            $scope.messages.push(message);
-            $scope.$digest();
-        });
-
-        // Handle user_entered event 
-        socket.on("user_entered", function(user){
-            var message = {
-                text: user.name + ' has joined the room.',
-                type: 'systemMessage'
-            };
-            $scope.messages.push(message);
-            $scope.$digest();
-        });
-    });
 });
-
-var nameModalCtrl = function($scope, $modalInstance){
-    $scope.user = {
-        nickname: ''
-    };
-    $scope.ok = function(){
-        $modalInstance.close($scope.user.nickname);
-    };
-};
-
-
-// jQuery way
-
-// var socket = io.connect('/');
-
-// socket.on("message", function(data){
-//     data = JSON.parse(data);
-//     $("#messages").append('<div class="'+data.type+'">'+data.message+'</div>');
-// });
-
-// $(function(){
-
-//     var inputMessage = $("#message");
-
-//     $("#send").click(function(){
-//         var data = {
-//             message : inputMessage.val(),
-//             type: 'userMessage'
-//         };
-
-//         socket.send(JSON.stringify(data));
-
-//         inputMessage.val('');
-//     });
-// });
